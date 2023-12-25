bed9aa6a2e8225cd9e739e9e4ee9eabc5c2c5973
NadiiaUa23 <hasiminka92@gmail.com>
2023-12-25 20:46:38 +0100

    js первая задача

--- js/task-01.js
+++ js/task-01.js
@@ -10,5 +10,11 @@
 
 const categoriesList = document.getElementById('categories');
 const items = categoriesList.querySelectorAll('li.item');
-console.log('Number of Categories: ${item.length}');
+console.log(`number of categories: ${items.length}`);
 
+
+items.forEach(item => {
+  const categoryName = item.querySelector('h2').textContent;
+  const categoryItemsCount = item.querySelectorAll('ul li').length;
+  console.log(`Category: ${categoryName}, Elements: ${categoryItemsCount}`);
+});

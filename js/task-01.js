efc01fcd017ef3732380b976fb4d48d5546b9c69
NadiiaUa23 <hasiminka92@gmail.com>
2023-12-28 23:14:37 +0100

    add task with css

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,13 @@
-// З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
-
-// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка 
-// елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
-
-/*На що буде звертати увагу ментор при перевірці:
-Кількість категорій, їх назва та кількість елементів отримані за допомогою властивостей і методів DOM-елементів
-Дані за кожною категорією були отримані й виведені в консоль у тілі циклу або методу forEach()*/
-
 const categoriesList = document.getElementById('categories');
 const items = categoriesList.querySelectorAll('li.item');
-console.log(`number of categories: ${items.length}`);
-
+console.log(`Number of categories: ${items.length}`);
 
 items.forEach(item => {
+    //Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+    //Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>)
   const categoryName = item.querySelector('h2').textContent;
+  // і кількість елементів у категорії (усіх <li>, вкладених у нього).
   const categoryItemsCount = item.querySelectorAll('ul li').length;
   console.log(`Category: ${categoryName}, Elements: ${categoryItemsCount}`);
 });
+
